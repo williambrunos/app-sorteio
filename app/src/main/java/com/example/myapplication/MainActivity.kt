ec7f210d3badc7e93a42b4a0c64e44faf44da0ce
William <williambrunosalesdepaulalima@gmail.com>
2024-12-05 20:08:54 -0300

    feat: adicionando códigos do app

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun onClick(view: View) {
+        val textResultado = findViewById<TextView>(R.id.text_resultado)
+        val numeroGerado = Random.nextInt(11)
+        textResultado.setText("Número gerado: $numeroGerado")
+    }
+}
